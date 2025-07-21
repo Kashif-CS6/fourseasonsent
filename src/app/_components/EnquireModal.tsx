bca32c6f0ce0000f047a37829430949d1a3c4813
Kashif-CS6 <kashifnabeel.cs@gmail.com>
2025-07-21 21:19:12 +0500

    change modal responsivness

--- src/app/_components/EnquireModal.tsx
+++ src/app/_components/EnquireModal.tsx
@@ -34,7 +34,7 @@ const EnquireModal: React.FC<ModalProps> = ({ open, onClose }) => {
       className="fixed inset-0 flex items-center justify-center bg-black/40 backdrop-blur-xs z-50"
       onMouseDown={(e) => e.target === overlayRef.current && onClose()}
     >
-      <div className="relative bg-white rounded-lg shadow-lg w-full max-w-3xl p-6">
+      <div className="relative bg-white rounded-lg shadow-lg w-full max-w-3xl p-6 max-h-[90vh] overflow-y-auto">
         <button
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
           onClick={onClose}
